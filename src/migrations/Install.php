99a2cff5242c7a6cee43ae24e15ef2d93df826c2
Josh Crawford <joshua@sgroup.com.au>
2022-04-30 18:53:41 +1000

    Add `archiveTableIfExists()` to install migration

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -11,22 +11,18 @@ class Install extends Migration
 
     public function safeUp(): bool
     {
-        if (!$this->db->tableExists('{{%assetcount}}')) {
-            $this->createTable('{{%assetcount}}', [
-                'id' => $this->primaryKey(),
-                'assetId' => $this->integer()->notNull(),
-                'count' => $this->integer()->defaultValue(0)->notNull(),
-                'dateCreated' => $this->dateTime()->notNull(),
-                'dateUpdated' => $this->dateTime()->notNull(),
-                'uid' => $this->uid(),
-            ]);
+        $this->archiveTableIfExists('{{%assetcount}}');
+        $this->createTable('{{%assetcount}}', [
+            'id' => $this->primaryKey(),
+            'assetId' => $this->integer()->notNull(),
+            'count' => $this->integer()->defaultValue(0)->notNull(),
+            'dateCreated' => $this->dateTime()->notNull(),
+            'dateUpdated' => $this->dateTime()->notNull(),
+            'uid' => $this->uid(),
+        ]);
 
-            $this->createIndex(null, '{{%assetcount}}', 'assetId', true);
-
-            $this->addForeignKey(null, '{{%assetcount}}', 'assetId', '{{%elements}}', 'id', 'CASCADE');
-
-            Craft::$app->getDb()->schema->refresh();
-        }
+        $this->createIndex(null, '{{%assetcount}}', 'assetId', true);
+        $this->addForeignKey(null, '{{%assetcount}}', 'assetId', '{{%elements}}', 'id', 'CASCADE');
 
         return true;
     }
