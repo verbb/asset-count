7e88ebfb29b1d129359d6c547057c14d2a248337
Josh Crawford <joshua@sgroup.com.au>
2022-07-12 16:08:43 +1000

    Translations

---
+++ src/translations/en/asset-count.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'Reset Asset Count' => 'Reset Asset Count',
+	'Asset Count Successfully Reset' => 'Asset Count Successfully Reset',
+	'View Count' => 'View Count',
+	'Asset Count' => 'Asset Count',
+	'Settings' => 'Settings',
+	'Show count on asset index pages' => 'Show count on asset index pages',
+	'Will add a new column for the asset count on the asset index pages' => 'Will add a new column for the asset count on the asset index pages',
+	'Ignore logged in users' => 'Ignore logged in users',
+	'Only increment counts if users are not logged in' => 'Only increment counts if users are not logged in',
+	'Ignore IP addresses' => 'Ignore IP addresses',
+	'Add one IP address per line to ignore them when incrementing counts' => 'Add one IP address per line to ignore them when incrementing counts',
+	'Asset count reset.' => 'Asset count reset.',
+];
