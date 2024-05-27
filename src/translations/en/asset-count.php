a25817ce2c5ec300a9140ba86338d7d5c653331d
Josh Crawford <joshua@sgroup.com.au>
2024-05-27 14:51:47 +1000

    Update English translations.

--- src/translations/en/asset-count.php
+++ src/translations/en/asset-count.php
@@ -1,8 +1,16 @@
 <?php
 
 return [
+  'Add one IP address per line to ignore them when incrementing counts.' => 'Add one IP address per line to ignore them when incrementing counts.',
+  'Asset Count' => 'Asset Count',
   'Asset count reset.' => 'Asset count reset.',
   'Asset Count Successfully Reset' => 'Asset Count Successfully Reset',
+  'Ignore IP addresses' => 'Ignore IP addresses',
+  'Ignore logged in users' => 'Ignore logged in users',
+  'Only increment counts if users are not logged in.' => 'Only increment counts if users are not logged in.',
   'Reset Asset Count' => 'Reset Asset Count',
+  'Settings' => 'Settings',
+  'Show count on asset index pages' => 'Show count on asset index pages',
   'View Count' => 'View Count',
+  'Will add a new column for the asset count on the asset index pages.' => 'Will add a new column for the asset count on the asset index pages.',
 ];
